9e09af5b0612ccd146f87f89ac69feb5268ae04a
Yogi Indragiri <g13.indra@gmail.com>
2020-09-19 22:11:27 +0700

    lala

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
                     <div class="col-md-8">
                         <h1>Selamat datang di Pesbuk</h1>
                         <h2 style="color: blue;">Hallo Ini Update dev lagi</h2>
+                        <p>halo</p>
                         <p>Bergabunglah bersama jutaan orang lainnya...</p>
                     </div>
                     <div class="col-md-4">
